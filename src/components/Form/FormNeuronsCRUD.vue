18db6de0a6640a085033d0dc953003c808d467f9
Ivan <45362427+Efcolipt@users.noreply.github.com>
2021-08-15 02:11:42 +0300

    added forms CRUD

---
+++ src/components/Form/FormNeuronsCRUD.vue
@@ -0,0 +1,18 @@
+<template>
+  <form class="form" @submit.prevent>
+    <div class="form-control">
+      <h2>Параметры датасета</h2>
+      <BaseInput type="text" placeholder="Название датасета" />
+      <BaseInput type="text" placeholder="Теги" />
+      <BaseInput type="text" placeholder="Размер датасета" />
+    </div>
+
+    <BaseButton>Сформировать</BaseButton>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "FormNeuronsCRUD",
+};
+</script>
---
+++ src/components/Form/FormTagsCRUD.vue
@@ -0,0 +1,16 @@
+<template>
+  <form class="form" @submit.prevent>
+    <div class="form-control">
+      <h2>Параметры тега</h2>
+      <BaseInput type="text" placeholder="Название Тега" />
+    </div>
+
+    <BaseButton>Сформировать</BaseButton>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "FormTagsCRUD",
+};
+</script>
