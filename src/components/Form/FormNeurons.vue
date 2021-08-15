c921b6c77278e0b70d4a21b7f2698eec8ac153b3
Ivan <45362427+Efcolipt@users.noreply.github.com>
2021-08-15 06:53:45 +0300

    fix form dataset

---
+++ src/components/Form/FormNeurons.vue
@@ -0,0 +1,49 @@
+<template>
+  <form class="form" @submit.prevent>
+    <div class="form-control">
+      <BaseInput
+        v-model="neuron.name"
+        type="text"
+        placeholder="Название датасета"
+      />
+      <BaseInput v-model="neuron.tags" type="text" placeholder="Теги" />
+      <BaseInput
+        v-model="neuron.memory"
+        type="text"
+        placeholder="Размер датасета"
+      />
+    </div>
+
+    <BaseButton @click.native="addNeuron"> Сформировать </BaseButton>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "FormNeurons",
+  data() {
+    return {
+      neuron: {
+        name: "",
+        tags: "",
+        memory: "",
+      },
+    };
+  },
+  methods: {
+    addNeuron() {
+      const neuron = Object.assign({}, this.neuron);
+      this.$store.dispatch("AddNeuron", neuron);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.form-control {
+  margin-bottom: 20px;
+}
+.form-control input {
+  margin-bottom: 10px;
+}
+</style>
