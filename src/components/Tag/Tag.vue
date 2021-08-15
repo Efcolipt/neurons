caf450b1703d9b49967cb1e0ac359470f830fbd4
Ivan <45362427+Efcolipt@users.noreply.github.com>
2021-08-15 06:54:49 +0300

    tags is done

--- src/components/Tag/Tag.vue
+++ src/components/Tag/Tag.vue
@@ -1,8 +1,18 @@
 <template>
-  <div class="tags">
-    <TagItem v-for="tag in tags" :key="tag.value">
-      {{ tag.value }}
-    </TagItem>
+  <div class="tags" v-if="tags.length > 0">
+    <div class="row flex">
+      <TagItem
+        v-for="tag in tags"
+        :key="tag"
+        :ref="`item${tag}`"
+        @click.native="addSortTags(tag)"
+      >
+        {{ tag }}
+      </TagItem>
+    </div>
+  </div>
+  <div v-else>
+    <p class="empty">Пока ничего тут нет :)</p>
   </div>
 </template>
 
@@ -15,8 +25,19 @@ export default {
   },
   props: {
     tags: {
-      type: Object,
-      default: () => {},
+      type: Array,
+      default: () => [],
+    },
+  },
+  methods: {
+    addSortTags(tag) {
+      const a = `item${tag}`;
+      if (!(this.$refs[a][0].$el.className.indexOf("tag-item--active") > -1)) {
+        this.$store.commit("AddSortTags", tag);
+      } else {
+        this.$store.commit("RemoveSortTags", tag);
+      }
+      this.$refs[a][0].$el.classList.toggle("tag-item--active");
     },
   },
 };
--- src/components/Tag/TagItem.vue
+++ src/components/Tag/TagItem.vue
@@ -11,3 +11,18 @@ export default {
   name: "TagItem",
 };
 </script>
+
+<style scoped>
+.tag-item--active {
+  color: #a4bad0;
+  background: #242f3d;
+}
+.tags-item {
+  padding: 7px 20px;
+  border-radius: 5px;
+  margin-right: 20px;
+  cursor: pointer;
+  margin-top: 10px;
+  border: 1px solid #304290;
+}
+</style>
